294d54ed221adaa2d7329986f4e7cc44b6922520
PARMMA <kornkanit_ma@cmu.ac.th>
2017-03-14 14:31:28 +0700

    update currency

---
+++ VersionControlLab/src/main/java/Currency.java
@@ -0,0 +1,12 @@
+/**
+ * Created by CAMT on 6/3/2560.
+ */
+public class Currency {
+
+
+	public double exchange(String sourceCurrency, double amount, String targetCurrency) {
+
+		return 0.0;
+
+	}
+}
--- VersionControlLab/src/main/java/main.java
+++
@@ -1,5 +0,0 @@
-/**
- * Created by CAMT on 13/3/2560.
- */
-public class main {
-}
